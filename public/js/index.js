46ae28f9fdf10898aacb0a2a528431468c6d8d48
Evan Desktop <evanjcleary@gmail.com>
2019-08-28 23:15:56 -0400

    was able to fix bug with js/css linking in handlebars and created post route for form on /view/manager

--- public/js/index.js
+++ public/js/index.js
@@ -1,114 +1,37 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
-var $loginBtn = $("#loginBtn");
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
+$(function () {
 
-// $("#loginBtn").on("click", function(){
-//   console.log("log in!")
-//   $.ajax({
-//     type: "GET",
-//     url: "view",
-//     success: function(result){
-//       window.location.href="selectView.html"
-//     }
-//   })
-// })
-
-
-
-
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+$("#create-project").on("submit", function (event) {
   event.preventDefault();
+  console.log("i've been clicked");
 
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+
+  var newProject = {
+    supervisor_id: parseInt($("#new-proj-supervisor").val()),
+    jobsite_id: parseInt($("#new-proj-jobsite").val()),
+    project_bid: parseInt($("#project-bid").val().trim())
   };
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
+  $.ajax("/api/jobs", {
+    type: "POST",
+    data: newProject 
+  }).then(
+    function () {
+      console.log("Created a new project");
+      location.reload();
+    }
+  )
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
+  console.log(newProject);
 
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
 
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
+});
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
 
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+
+
+
+
+
+
+});
